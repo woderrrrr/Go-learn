289cdb1a9c06fc4d89c0b8ca7f834e662e79004e
z2528509654 <z2528509654@163.com>
2024-06-18 15:40:57 +0800

    first

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("hello, world\n")
+	a := []int{1, 2, 3}
+	b := make([]int, 3)
+	b = append(b, 1)
+	fmt.Printf("%T,%v\n", a, a)
+	fmt.Printf("%T,%v\n", b, b)
+}
